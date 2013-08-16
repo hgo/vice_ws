f93e28e334aec1556c76a4ae3eaf7e4a438c8fe3
hgo <guvenozyurt@gmail.com>
2013-08-17 01:21:12 +0300

    socket io - prod mode

--- web.js
+++ web.js
@@ -54,12 +54,27 @@ var server = http.createServer(app).listen(app.get('port'), function(){
  connections = {},
  onlineAgents = {},
  io = require('socket.io').listen(server,{log:false});
-/* 
+ 
 io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+
+// enable all transports (optional if you want flashsocket support, please note that some hosting
+// providers do not allow you to create servers that listen on a port different than 80 or their
+// default port)
+io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+io.set("polling duration", 10); 
 });
- */
+
 /********** socket.io work ***************/
 
 function deleteOnlineAgent(socket){
